34133f3bb1a207970dbd484ad74444ccd741e572
Sarah Princes <121565426+sarahprinces007@users.noreply.github.com>
2023-10-27 16:26:48 +0530

    Update calc.java

--- src/com/edubridge/app1/calc.java
+++ src/com/edubridge/app1/calc.java
@@ -3,11 +3,13 @@ package com.edubridge.app1;
 public class calc {
 	public static void square(int a) {
 		System.out.println(a*a);
-		
+	//nothing	
 	}
 	public static void cube(int a) {
 		System.out.println(a*a*a);
 		
 	}
+	
+	
 
 }
