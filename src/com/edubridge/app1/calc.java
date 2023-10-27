606e04f11004eb30f6a0615bffbff665d5a93242
sarahprinces007 <sarahprincesgurunadham@gmail.com>
2023-10-27 16:23:44 +0530

    first commit

--- src/com/edubridge/app1/calc.java
+++ src/com/edubridge/app1/calc.java
@@ -1,8 +1,13 @@
 package com.edubridge.app1;
 
 public class calc {
-	public static void name(int a) {
+	public static void square(int a) {
 		System.out.println(a*a);
+		
+	}
+	public static void cube(int a) {
+		System.out.println(a*a*a);
+		
 	}
 
 }
